be47d515b21583d716e044b3aee67024631522c5
mbsalah <mbsalah@ncsu.edu>
2025-03-10 16:02:48 -0400

    Fixing hacker mode button

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("hackerModeToggle").addEventListener("click", function() {
         document.body.classList.toggle("hacker-mode");
     });
-});
+}); 
